5cbf9c2df9f8a0570464c73b4c36d920ba20f33a
Zinniie <ezinne.blessing@yahoo.com>
2022-06-22 20:32:47 +0100

    Django_Crud

--- I4G009787TZ9/blog/urls.py
+++ I4G009787TZ9/blog/urls.py
@@ -1,18 +1,13 @@
-import imp
-from unicodedata import name
 from django.urls import path
-
-from I4G009787TZ9.blog.apps import BlogConfig
-from .views import PostListView,PostCreateView,PostDetailView,PostUpdateView,PostDeleteView
+from . import views
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="all"),
-    path("create/", PostCreateView.as_view(), name="post_create"),
-    path("read/<slug:slug>", PostDetailView.as_view(), name="post_detail"),
-    path("update/<slug:slug>", PostUpdateView.as_view(), name="post_update"),
-    path("delete/<slug:slug>", PostDeleteView.as_view(), name="post_delete"),
-    
-]
+    path("", views.PostListView.as_view(), name="all"),
+    path("create/", views.PostCreateView.as_view(), name="post_create"),
+    path("delete/<slug:slug>", views.PostDeleteView.as_view(), name="post_delete"),
+    path("update/<slug:slug>", views.PostUpdateView.as_view(), name="post_update"),
+    path("read/<slug:slug>", views.PostDetailView.as_view(), name="post_detail"),
 
+]
